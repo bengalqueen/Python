58a6ef747ab4ad67f34ad6ff2e006ce4e17cf002
shreyadutta2505@gmail.com <shreyadutta2505@gmail.com>
2026-03-11 00:21:54 +0530

    update file

---
+++ nthPrimeNo.py
@@ -0,0 +1,24 @@
+# Print Nth Prime No.
+
+def is_prime(n):
+    if n <= 1:
+        return False
+    else:
+        for i in range(2, int(n**0.5)+1):
+            if n % i == 0:
+                return False
+        return True
+    
+def nth_prime(n):
+    count = 0
+    num = 1
+
+    while count < n:
+        num += 1
+        if is_prime(num):
+            count += 1
+    return num
+
+n = int(input("Enter no:"))
+print("Nth prime number is:", nth_prime(n))
+        
